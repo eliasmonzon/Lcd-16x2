5eddd0792cc71cad047895f8ce5dec0a261474ac
electronica y diagramas <151683295+eliasmonzon@users.noreply.github.com>
2024-04-15 19:38:16 -0300

    Update lcdscroll.py

--- Ejemplos/lcdscroll.py
+++ Ejemplos/lcdscroll.py
@@ -9,17 +9,19 @@ lcd = LCD_16x2(rs_pin=16, e_pin=17, d4_pin=10, d5_pin=11, d6_pin=12, d7_pin=13)
 texto_completo = "THE WORLD IS YOURS"
 
 while True:
-    # Limpiar la pantalla al inicio de cada ciclo
+  
     
     
     # Simular el desplazamiento del texto
-    for i in range(len(texto_completo) - 15):  # Longitud del texto menos la longitud de la pantalla (16 caracteres)
+    for i in range(len(texto_completo) ):  # Longitud del texto menos la longitud de la pantalla (16 caracteres)
         # Obtener la subcadena desplazada
         
         subcadena_visible = texto_completo[i:i+16]  # Mostrar solo los siguientes 16 caracteres
-        
+        #limpia la pantalla
+        lcd.clear()
         # Mostrar la subcadena en la pantalla
         lcd.display_string(subcadena_visible, 1, 0)
         
         # Esperar un breve período de tiempo para el efecto de desplazamiento
-        utime.sleep(0.7)
+        utime.sleep(1)
+
